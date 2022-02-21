cbdbfd65b5c14043bcea97fd86e1db5de19735fd
bluemint56 <mint.or.mint-7888@outlook.jp>
2022-02-21 09:27:22 +0900

    advice-in

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -23,7 +23,7 @@
   <input type="hidden" name="updated_at">
     <p>日付を入力してください</p>
   <div class="f-search">
-  <input type="text" name="updated_at" class="frame" placeholder="例: 2022-01-01">
+  <input type="date" name="updated_at" class="frame" placeholder="例: 2022-01-01">
   <button type="submit" class="search-btn">検索</button>
   </div>
 </form>
